fdaba579508093940324c2f8eda616cfe8775ed3
mshanemc <shane.mclaughlin@salesforce.com>
2019-11-13 13:04:40 -0600

    update package per KellyW

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,3 @@
 sfdx force:org:create -d 1 -s -f config/project-scratch-def.json
-sfdx force:package:install --package 04t1U0000068gtE -w 20
+sfdx force:package:install --package 04t1U000007LhbH -w 20
 sfdx force:org:open
